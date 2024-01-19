beb1f00841279324ea0a326f8e7fe778e60a3988
Yaroslav Shypunov <yaroslav.shypunov@polcode.net>
2024-01-19 10:45:00 -0500

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Testowy blog</h1>
+      <h1>Testowy blog.</h1>
       <p>aidevs</p>
       <p>aidevs</p>
       <p>aidevs</p>
